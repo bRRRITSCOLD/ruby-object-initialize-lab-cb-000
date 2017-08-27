30ed16a173bb957f966bc46d2f258d17c0496bf2
Blaine Richardson <blainerrichardson@gmail.com>
2017-08-27 01:58:12 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,23 +1,7 @@
 class Person
 
-  def intialize(name,job)
+  defclass Person
+  def initialize(name)
     @name = name
-    @job = job
-  end
-
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def job
-    @job
-  end
-
-  def job=(new_job)
-    @job = new_job
   end
 end
